f27c40c602ef8de2e977648dddc0e9033f13d445
Takahito Nakano <kano@ponko2.jp>
2026-09-13 05:00:00 +0900

    chore: replace src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use topcoat::{
+    Result,
+    router::{Router, RouterBuilderDiscoverExt, page},
+    view::{View, component, view},
+};
+
+#[tokio::main]
+async fn main() {
+    topcoat::start(Router::builder().discover().build())
+        .await
+        .unwrap();
+}
+
+#[page("/")]
+async fn home() -> Result<impl View> {
+    Ok(view! {
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>"Hello world"</title>
+                topcoat::dev::script()
+            </head>
+            <body>hello(name: "World")</body>
+        </html>
+    })
+}
+
+#[component]
+async fn hello(name: &str) -> Result<impl View> {
+    Ok(view! {
+        <h1>
+            "Hello, "
+            (name)
+            "!"
+        </h1>
+    })
 }
